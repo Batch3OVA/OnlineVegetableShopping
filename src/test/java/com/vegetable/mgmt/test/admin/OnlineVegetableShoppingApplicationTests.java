5e65d6023f55eddb492cdc3b4bbec7927a3e1628
Batch3Project <rohithgcgk@gmail.com>
2021-06-18 00:15:41 +0530

    Create OnlineVegetableShoppingApplicationTests.java

---
+++ src/test/java/com/vegetable/mgmt/test/admin/OnlineVegetableShoppingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vegetable.mgmt.test.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineVegetableShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+		
+		
+	}
+
+}
