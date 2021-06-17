7c02ee6cf6c75c9908aa4931d830087b8350d310
Batch3Project <rohithgcgk@gmail.com>
2021-06-18 00:11:34 +0530

    Update AdminController.java

--- src/main/java/com/vegetable/mgmt/controller/AdminController.java
+++ src/main/java/com/vegetable/mgmt/controller/AdminController.java
@@ -1,22 +1,49 @@
 package com.vegetable.mgmt.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vegetable.mgmt.entities.Admin;
+import com.vegetable.mgmt.service.impl.IAdminServiceImpl;
 
 @RestController
+@RequestMapping("/Admin")
 @Configuration
 public class AdminController {
-
-	@GetMapping("/view-admin")
-	public Admin viewAdmin(@RequestBody Admin admin) {
-		Admin viewAdmin = new Admin();
-		viewAdmin.setAdminId(123);
-		return viewAdmin;
-
+@Autowired
+IAdminServiceImpl service;
+	@PostMapping()
+	public ResponseEntity<?> getAddAdmin(@RequestBody Admin admin){
+		return new ResponseEntity<String> (service.addAdmin(admin),HttpStatus.CREATED);
+		
 	}
-
+	@PutMapping("{adminId}")
+	public ResponseEntity<?> getUpdateAdminDetails(@RequestBody Admin admin,@PathVariable int adminId){
+		return new ResponseEntity<String> (service.updateAdminDetails(admin,adminId),HttpStatus.ACCEPTED);
+		}
+	@DeleteMapping("{adminId}")
+	public ResponseEntity<?> getRemoveAdmin(@PathVariable int adminId){
+		return new ResponseEntity<String>(service.removeAdmin(adminId),HttpStatus.OK);
+	}
+	@GetMapping()
+	public List<Admin> getViewAdmin(){
+		return service.viewAdmin();
+	}
+	@GetMapping("{adminId}")
+	public Admin ViewAdmin(@PathVariable int adminId){
+		return service.viewAdminDetails(adminId);
+	}
 }
+
