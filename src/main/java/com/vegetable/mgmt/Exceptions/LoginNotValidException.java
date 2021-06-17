790b8577a8cc3591dda7d732ed8cc0a64cb0a5c9
Batch3Project <rohithgcgk@gmail.com>
2021-06-18 00:14:53 +0530

    Create LoginNotValidException.java

---
+++ src/main/java/com/vegetable/mgmt/Exceptions/LoginNotValidException.java
@@ -0,0 +1,9 @@
+package com.vegetable.mgmt.Exceptions;
+
+public class LoginNotValidException extends RuntimeException{
+
+	public LoginNotValidException(String msg) {
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+}
